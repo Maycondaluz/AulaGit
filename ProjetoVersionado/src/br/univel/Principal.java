df1e7b64d479ac3fe955c3acdc6b25d268ce8579
Maycon Vinicius da Luz <maycondaluz2010@hotmail.com>
2016-03-30 20:10:54 -0300

    Alteraçao apos a criaçao da Tag

--- ProjetoVersionado/src/br/univel/Principal.java
+++ ProjetoVersionado/src/br/univel/Principal.java
@@ -7,6 +7,10 @@ public static void main(String[] args) {
 	System.out.println("Ola mundo");
 	System.out.println("teste");
 	System.out.println("3 teste");
+	
+	String word = "Word";
+	String msg = "Hello" + word +"!";
+	System.out.println(msg);
 }
 
 }
